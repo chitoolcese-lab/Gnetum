6c09fc359532971bd0ade18f54c1fd96c7ab4fd8
chitoolcese-lab <chitoolcese@gmail.com>
2026-09-06 19:32:30 -0500

    Update stonecutter.gradle.kts

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -18,9 +18,9 @@ stonecutter parameters {
         else -> "import com.mojang.blaze3d.platform.DestFactor; import com.mojang.blaze3d.platform.SourceFactor;"
     }
     swaps["import_delta_tracker"] = when {
-        swaps["import_delta_tracker"] = when {
-        eval(current.version, ">=1.21") -> "import net.minecraft.client.DeltaTracker;"
-    }
+            eval(current.version, ">=1.21") -> "import net.minecraft.client.DeltaTracker;"
+            else -> "// No delta tracker in this version"
+        }
     swaps["src_factor"] = when {
         eval(current.version, ">=26.2") -> "BlendFactor.$1"
         else -> "SourceFactor.$1"
